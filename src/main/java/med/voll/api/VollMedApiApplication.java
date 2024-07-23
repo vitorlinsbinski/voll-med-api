82a9fbcf661b40f5658133ea9a897277f3033905
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-07-23 15:11:40 -0400

    first commit

---
+++ src/main/java/med/voll/api/VollMedApiApplication.java
@@ -0,0 +1,13 @@
+package med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VollMedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VollMedApiApplication.class, args);
+    }
+
+}
